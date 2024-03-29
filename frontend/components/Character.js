46f39cb0a6560abade469e0dd06755a293dd8fb3
Ashley Lago <ashl3ylag0@gmail.com>
2024-03-29 02:17:53 -0400

    Finished Project

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character({starChara, hmewrd}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [isHomeworldRendering, setIsHomeworldRendering] = useState(false);
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggleHomeworld = () => {
+    setIsHomeworldRendering(!isHomeworldRendering);
+  }
   return (
-    <div>
+    <div onClick={toggleHomeworld} className="character-card">
+      <h3 className="character-name">{starChara}</h3>
+      {isHomeworldRendering 
+        ? <p>Planet: <span className="character-planet">{hmewrd}</span></p> 
+        : console.log("no homeworld is showing")
+      }
       {/* Use the same markup with the same attributes as in the mock */}
-    </div>
+    </div> 
   )
 }
 
